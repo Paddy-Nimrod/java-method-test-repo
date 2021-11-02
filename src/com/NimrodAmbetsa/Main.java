e5ec9108235acbaa64e92d98d15aade1348a1f60
unknown <n.ambetsa21@gmail.com>
2021-11-02 03:48:27 +0300

    first commit of the java program method

---
+++ src/com/NimrodAmbetsa/Main.java
@@ -0,0 +1,50 @@
+package com.NimrodAmbetsa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int highScore = calculateScore(true, 5000, 5, 100);
+        System.out.println("Your high score marks is = " + highScore);
+        calculateScore(true, 10000, 8, 200);
+
+        int firstInput = calculateHighScorePosition(70);
+        displayHighScorePosition("Nimrod", firstInput);
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("Your final score is = " + finalScore);
+            return finalScore;
+        }
+        return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+//        if (score >= 1000) {
+//            return 1;
+//        } else if (score >= 500) {
+//            return 2;
+//        } else if (score >= 100) {
+//            return 3;
+//        }
+//        return 4;
+
+        int position = 4;
+        if (score >= 1000) {
+            position = 1;
+        } else if (score >= 500) {
+            position = 2;
+        } else if (score >= 100) {
+            position = 3;
+        }
+        return position;
+    }
+}
